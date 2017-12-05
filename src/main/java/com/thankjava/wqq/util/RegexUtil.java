8d7a1e545bd8c111d6700635adafd1e55360ac6a
Acexy <thankjava@foxmail.com>
2017-12-05 22:15:36 +0800

    调整依赖

--- src/main/java/com/thankjava/wqq/util/RegexUtil.java
+++ src/main/java/com/thankjava/wqq/util/RegexUtil.java
@@ -7,22 +7,23 @@ import com.thankjava.wqq.consts.DataResRegx;
 
 public class RegexUtil {
 
-	/**
-	 * 使用正则去匹配返回的数据
-	 * @param content
-	 * @param regx
-	 * @return
-	 */
-	public static String[] doRegex(String content, DataResRegx regx){
-		Pattern pattern = Pattern.compile(regx.regx);
-		Matcher matcher = pattern.matcher(content);
-		if(matcher.find()){
-			String[] values = new String[matcher.groupCount()];
-			for(int i = 1 ; i < matcher.groupCount(); i ++){
-				values[i - 1] = matcher.group(i);
-			}
-			return values;
-		}
-		return null;
-	}
+    /**
+     * 使用正则去匹配返回的数据
+     *
+     * @param content
+     * @param regx
+     * @return
+     */
+    public static String[] doRegex(String content, DataResRegx regx) {
+        Pattern pattern = Pattern.compile(regx.regx);
+        Matcher matcher = pattern.matcher(content);
+        if (matcher.find()) {
+            String[] values = new String[matcher.groupCount()];
+            for (int i = 1; i < matcher.groupCount(); i++) {
+                values[i - 1] = matcher.group(i);
+            }
+            return values;
+        }
+        return null;
+    }
 }
--- src/main/java/com/thankjava/wqq/util/WqqEncryptor.java
+++ src/main/java/com/thankjava/wqq/util/WqqEncryptor.java
@@ -12,52 +12,53 @@ import com.thankjava.wqq.core.action.LoginAction;
 
 public class WqqEncryptor {
 
-	private static final Logger logger = LoggerFactory.getLogger(LoginAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoginAction.class);
 
-	/**
-	 * 计算一些接口的hash值</p>
-	 * Function: hash</p>
-	 * <p>Description:</p>
-	 * 
-	 * @author zhaoxy@thankjava.com
-	 * @date 2016年12月21日 下午6:25:04
-	 * @version 1.0
-	 * @param uin
-	 * @param ptwebqq
-	 * @return
-	 */
-	public static String hash(String uin, String ptwebqq) {
+    /**
+     * 计算一些接口的hash值</p>
+     * Function: hash</p>
+     * <p>Description:</p>
+     *
+     * @param uin
+     * @param ptwebqq
+     * @return
+     * @author zhaoxy@thankjava.com
+     * @date 2016年12月21日 下午6:25:04
+     * @version 1.0
+     */
+    public static String hash(String uin, String ptwebqq) {
 
-		try {
-			String jsSources = IOUtils.toString(SourceLoader.getResourceAsInputStream("hash.js"), "utf-8");
-			ScriptEngineManager scriptEMgr = new ScriptEngineManager();
-			ScriptEngine engine = scriptEMgr.getEngineByMimeType("application/javascript");
-			engine.eval(jsSources);
-			Invocable invocable = (Invocable) engine;
-			// 调用js
-			return (String) invocable.invokeFunction("hash", uin, ptwebqq);
-		} catch (Exception e) {
-			logger.error("计算hash值异常", e);
-		}
+        try {
+            String jsSources = IOUtils.toString(SourceLoader.getResourceAsInputStream("hash.js"), "utf-8");
+            ScriptEngineManager scriptEMgr = new ScriptEngineManager();
+            ScriptEngine engine = scriptEMgr.getEngineByMimeType("application/javascript");
+            engine.eval(jsSources);
+            Invocable invocable = (Invocable) engine;
+            // 调用js
+            return (String) invocable.invokeFunction("hash", uin, ptwebqq);
+        } catch (Exception e) {
+            logger.error("计算hash值异常", e);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * 检查二维码时，用于计算ptqrtoken
-	* <p>Function: hashForCheckQrStatus</p>
-	* <p>Description: </p>
-	* @author zhaoxy@thankjava.com
-	* @date 2017年2月13日 下午6:11:06
-	* @version 1.0
-	* @param str
-	* @return
-	 */
-	public static String hashForCheckQrStatus(String str) {
-		long hash = 0;
-		for (int i = 0, length = str.length(); i < length; i++) {
-			hash += hash * 32 + str.charAt(i);
-		}
-		return String.valueOf(2147483647 & hash);
-	}
+    /**
+     * 检查二维码时，用于计算ptqrtoken
+     * <p>Function: hashForCheckQrStatus</p>
+     * <p>Description: </p>
+     *
+     * @param str
+     * @return
+     * @author zhaoxy@thankjava.com
+     * @date 2017年2月13日 下午6:11:06
+     * @version 1.0
+     */
+    public static String hashForCheckQrStatus(String str) {
+        long hash = 0;
+        for (int i = 0, length = str.length(); i < length; i++) {
+            hash += hash * 32 + str.charAt(i);
+        }
+        return String.valueOf(2147483647 & hash);
+    }
 }
