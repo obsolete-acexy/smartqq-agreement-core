3b7c3b1e265083150a468d6321e1fb1a007965cd
Acexy <thankjava@foxmail.com>
2018-03-08 13:28:23 +0800

    调整1.1.0的测试代码案例

--- src/test/java/com/thankjava/wqq/test/qq/TestSmartQQNewVersion.java
+++ src/test/java/com/thankjava/wqq/test/qq/TestSmartQQNewVersion.java
@@ -2,10 +2,8 @@ package com.thankjava.wqq.test.qq;
 
 import com.thankjava.wqq.SmartQQClient;
 import com.thankjava.wqq.SmartQQClientBuilder;
-import com.thankjava.wqq.entity.msg.PollMsg;
 import com.thankjava.wqq.extend.ActionListener;
 import com.thankjava.wqq.extend.CallBackListener;
-import com.thankjava.wqq.extend.NotifyListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
