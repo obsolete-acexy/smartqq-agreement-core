f1964f99e804d015cf2f61016c227d71c7cab4f5
Acexy <thankjava@foxmail.com>
2018-03-12 10:31:42 +0800

    增加一处空指针提示

--- src/main/java/com/thankjava/wqq/SmartQQClientBuilder.java
+++ src/main/java/com/thankjava/wqq/SmartQQClientBuilder.java
@@ -10,91 +10,93 @@ import com.thankjava.wqq.factory.ActionFactory;
 
 /**
  * SmartQQClient 实力创建工具指南
- * 
- * @author acexy
  *
+ * @author acexy
  */
 public class SmartQQClientBuilder {
 
-	private SmartQQClientBuilder() {
-	}
+    private SmartQQClientBuilder() {
+    }
 
-	private static WQQClient client = null;
+    private static WQQClient client = null;
 
-	/**
-	 * 声明需要自定义参数化 SmartQQClient
-	 * 
-	 * @param notifyListener
-	 * @return
-	 */
-	public static SmartQQClientBuilder custom(NotifyListener notifyListener) {
+    /**
+     * 声明需要自定义参数化 SmartQQClient
+     *
+     * @param notifyListener
+     * @return
+     */
+    public static SmartQQClientBuilder custom(NotifyListener notifyListener) {
 
-		client = new WQQClient();
+        client = new WQQClient();
 
-		if (notifyListener == null) {
-			throw new NullPointerException("notifyListener can not be null");
-		}
+        if (notifyListener == null) {
+            throw new NullPointerException("notifyListener can not be null");
+        }
 
-		ReflectHelper.setFieldVal(client, "listener", notifyListener);
+        ReflectHelper.setFieldVal(client, "listener", notifyListener);
 
-		return new SmartQQClientBuilder();
-	}
+        return new SmartQQClientBuilder();
+    }
 
-	/**
-	 * 设置单次请求与异常的最大重试次数
-	 * 
-	 * @param times
-	 * @return
-	 */
-	public SmartQQClientBuilder setExceptionRetryMaxTimes(int times) {
-		if (times < 1) {
-			throw new IllegalArgumentException("exceptionRetryMaxTimes should >= 1");
-		}
-		ConfigParams.EXCEPTION_RETRY_MAX_TIME = times;
-		return this;
-	}
+    /**
+     * 设置单次请求与异常的最大重试次数
+     *
+     * @param times
+     * @return
+     */
+    public SmartQQClientBuilder setExceptionRetryMaxTimes(int times) {
+        if (times < 1) {
+            throw new IllegalArgumentException("exceptionRetryMaxTimes should >= 1");
+        }
+        ConfigParams.EXCEPTION_RETRY_MAX_TIME = times;
+        return this;
+    }
 
-	/**
-	 * 登录成功后立即获取一些信息并缓存下来(自己的信息，讨论组，好友信息)
-	 * 
-	 * @return
-	 */
-	public SmartQQClientBuilder setAutoGetInfoAfterLogin() {
-		ConfigParams.INIT_LOGIN_INFO = true;
-		return this;
-	}
+    /**
+     * 登录成功后立即获取一些信息并缓存下来(自己的信息，讨论组，好友信息)
+     *
+     * @return
+     */
+    public SmartQQClientBuilder setAutoGetInfoAfterLogin() {
+        ConfigParams.INIT_LOGIN_INFO = true;
+        return this;
+    }
 
     /**
      * 设置若登录二维码过期则自动重新拉取下一张二维码
+     *
      * @return
      */
-	public SmartQQClientBuilder setAutoRefreshQrcode() {
+    public SmartQQClientBuilder setAutoRefreshQrcode() {
         ConfigParams.AUTO_REFRESH_QR_CODE = true;
         return this;
     }
 
-	/**
-	 * 创建SmartQQClient实例并登录
-	 * 
-	 * @param getQrListener
-	 *            获取到登录二维码后将进行回调
-	 * @param loginListener
-	 *            登录完毕后的回调函数，函数会返回一个LoginResult的登录状态反馈值
-	 * @return
-	 */
-	public SmartQQClient create(final CallBackListener getQrListener, final CallBackListener loginListener) {
+    /**
+     * 创建SmartQQClient实例并登录
+     *
+     * @param getQrListener 获取到登录二维码后将进行回调
+     * @param loginListener 登录完毕后的回调函数，函数会返回一个LoginResult的登录状态反馈值
+     * @return
+     */
+    public SmartQQClient create(final CallBackListener getQrListener, final CallBackListener loginListener) {
 
-		LoginAction loginAction = ActionFactory.getInstance(LoginAction.class);
+        if (getQrListener == null || loginListener == null) {
+	        throw new NullPointerException("listener can not be null");
+        }
 
-		loginAction.login(getQrListener, new CallBackListener() {
+        LoginAction loginAction = ActionFactory.getInstance(LoginAction.class);
 
-		    @Override
-			public void onListener(ActionListener actionListener) {
-				loginListener.onListener(actionListener);
-			}
+        loginAction.login(getQrListener, new CallBackListener() {
 
-		});
-		
-		return client;
-	}
+            @Override
+            public void onListener(ActionListener actionListener) {
+                loginListener.onListener(actionListener);
+            }
+
+        });
+
+        return client;
+    }
 }
